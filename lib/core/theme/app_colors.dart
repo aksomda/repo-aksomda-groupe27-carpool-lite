78d3d0da10c692931966903cb08225bddba32bb2
Mayeny_Cherif <mayenycherif679@gmail.com>
2026-09-14 15:30:19 -0700

    fusion

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,1 +1,26 @@
-// TODO: Palette de couleurs centralisée.
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color textPrimary = Color(0xFF1A1A1A);
+
+  static const Color textSecondary = Color(0xFF757575);
+
+  static const Color navy = Color(0xFF14213D);
+
+  static const Color error = Color(0xFFD32F2F);
+
+  static const Color inputFill = Color(0xFFF5F6F8);
+
+  static const Color surface = Colors.white;
+
+  static const Color accentYellow = Color(0xFFFFC107);
+
+  static const Color rating = Color(0xFFFFB300);
+
+  // Couleurs utilisées dans HomeScreen
+  static const Color background = Color(0xFFF8F9FA);
+
+  static const Color primary = Color(0xFF56AB2F);
+
+  static const Color border = Color(0xFFE0E0E0);
+}
